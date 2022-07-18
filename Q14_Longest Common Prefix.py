fdc0872c323d121ccde604ebc1cc32c9a3fc68ab
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-18 16:36:36 +0530

    Update Q14_Longest Common Prefix.py

--- Q14_Longest Common Prefix.py
+++ Q14_Longest Common Prefix.py
@@ -1,6 +1,11 @@
 strs = ["flower","flow","flight"]
 
 #initial solution
+
+"""initialize the short as strs first element and prefix as an empty string and in forloop print
+ the index of every character in short and compare with the strs elements character with short char as follows
+and append the same char in prefix and print the prefix at the end of the forloop"""
+
 short=strs[0]
 prefix=""
 for i in range(len(short)):
@@ -11,6 +16,12 @@ for i in range(len(short)):
 print(prefix)
 
 #improved solution
+
+"""
+Initialize the output as empty string to append the similar char in forloop using zip function
+it will zip the char by char for first char in every words in a list and using if condition convert the char as set and length is equal to 
+1 then it will add the char in output and finally return the similar char in output 
+"""
 
 output = ""
 for i in zip(*strs):
