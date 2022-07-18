85317c22f1995ccceb4f7f3e6938b4a95f2db0fe
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-18 15:57:06 +0530

    Update Q1_Two sum.py

--- Q1_Two sum.py
+++ Q1_Two sum.py
@@ -3,6 +3,10 @@ target = 9
 
 #initial solution
 
+"""At first forloop it will start to print the index of element in n and second forloop it start print
+index of second element and compare using if condition by add elemnt first and second is equal to
+target it print its index as output"""
+
 for i in range(len(n)):
     for j in range(i+1, len(n)):
         if n[i] + n[j] == target:
@@ -10,6 +14,10 @@ for i in range(len(n)):
 
 
 #improved solution
+""" In a first forloop it prints the index position of a element in a list n.And in if condition target is sub
+with every element in n if not in intially declared dictinory result it appends in results both element and its index
+else it prints its indexposition of the result and n"""
+
 result = {}
 for i in range(len(n)):
     if target - n[i] not in result:
