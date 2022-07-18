6cdf475f4d88b95287daefff553947c29c0c4de9
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-18 16:58:23 +0530

    Update Q26_Remove Duplicates from Sorted Array.py

--- Q26_Remove Duplicates from Sorted Array.py
+++ Q26_Remove Duplicates from Sorted Array.py
@@ -1,7 +1,14 @@
-#initial solution
 
 #nums = [1,1,2]
 nums = [0,0,1,1,1,2,2,3,3,4]
+
+#initial solution
+
+"""
+Initialize the empty list and in forloop print the nums one by one and in if condition the char or int not in result it will
+append in the results after complete have to clear the nums and to append result values in nums as the problem's 
+requirement and print the length of nums. 
+"""
 
 result = []
 for i in nums:
@@ -14,11 +21,15 @@ print(len(nums))
 
 #improved solution
 
+"""
+initialize k = 0 and in forloop check the char is not equal to the nums and increment the k as condition is true
+and finally prints the k + 1 to print the length of the removed duplicate length of nums.
+"""
+
 k = 0
 
 for n in nums:
     if n != nums[k]:
         k += 1
-        nums[k] = n
-
+        
 print(k + 1)
