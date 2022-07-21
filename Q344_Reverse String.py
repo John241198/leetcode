24e27d187b7e88aed8c02c13ff2212a1b8c33084
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-21 18:28:16 +0530

    Update Q344_Reverse String.py

--- Q344_Reverse String.py
+++ Q344_Reverse String.py
@@ -2,6 +2,11 @@ s = ["H", "a", "n", "n", "a", "h"]
 
 #initial solution
 """
+initial empty list results and join the s as string and reverse it using slicing
+and append the reversed char in result using forloop and clear the s input list
+and append the result to s using forloop obtained the expected result as reversed string
+in the list
+"""
 result = []
 out = "".join(s)
 output = out[::-1]
@@ -10,8 +15,12 @@ for i in output:
     s.clear()
 for i in result:
     s.append(i)
-"""
+
 #improved solution
+"""
+using slice reverse the input list and clear the input string and using extended add the temp list in s
+"""
+
 temp = s[::-1]
 s.clear()
 s.extend(temp)
