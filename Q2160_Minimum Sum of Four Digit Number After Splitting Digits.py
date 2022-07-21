0b08f261217901f2ef6c2bd205dbc931fe95222f
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-21 10:16:16 +0530

    Update Q2160_Minimum Sum of Four Digit Number After Splitting Digits.py

--- Q2160_Minimum Sum of Four Digit Number After Splitting Digits.py
+++ Q2160_Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -2,6 +2,12 @@
 num = 2932
 
 #intital solution
+
+"""
+Initially convert the num to string and sort the string and using slicing alternate number to new number's and convert
+into integer and add the both integer.
+"""
+
 s = str(num)
 s = "".join(sorted(s))
 new1 = s[0] + s[2]
@@ -10,6 +16,14 @@ new2 = s[1] + s[3]
 print(int(new1) + int(new2))
 
 #improved solution
+"""
+initially num_list as empty list in while loop if num greater then 0 we
+append the num by modulus operator by 10 to get last digit and append in
+num_list as remainder and then using floor division we can remove the append 
+num in num and continue the appending reaming nums and sorted the list and 
+added in digits in separate variable and we add digits alternatively. 
+"""
+
 num_list = []
 while num > 0:
     num_list.append(num % 10)
