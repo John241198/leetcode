a61ead7bf624996b00b48f8fbbaf674b60ba4f58
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-21 15:32:40 +0530

    Update Q27_Remove Element.py

--- Q27_Remove Element.py
+++ Q27_Remove Element.py
@@ -2,14 +2,18 @@ nums=[3,2,2,3]
 val=2
 
 #initial solution
+"""
+Initially assigned result is zero in for loop if nums are not equal to
+the val then we increment the result and we finally get the output as 
+length of nums in result.
+"""
 
 result = 0
 for n in nums:
     if n is not val:
-        nums[result] = n
         result+=1
 print(result)
 
 
-#improved solution
+
 
