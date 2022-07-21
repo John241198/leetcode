f4b5b39d2da20cc741dde78b080258c643b09b7c
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-21 09:53:03 +0530

    Update Q125_Valid Palindrome.py

--- Q125_Valid Palindrome.py
+++ Q125_Valid Palindrome.py
@@ -1,6 +1,11 @@
 s ="race a car"
 
 #initial solution
+"""
+In the first forloop append the character in out empty list using lower method and in second
+for loop validate the non-alphanumeric character in char and compare and remove in out
+and in if condition input is equal to reverse string then its true else it false.
+"""
 out=[]
 char=[',',':']
 for i in s:
@@ -18,7 +23,11 @@ else:
 
 
 #improved solution
-
+"""
+using lower method convert the string to lower and in char it as all alphabet and nums as testcase contains
+compare it with forloop with s and char if it as it will append string list and we can join the string to remove
+white space and using if condition input is equal to reverse string then its true else false.
+"""
 s = s.lower()
 char = 'abcdefghijklmnopqrstuvwxyz0123456789'
 string = []
