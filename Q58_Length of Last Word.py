dc748baeb28e5dd7003b0d3ff608bc6d6b87262b
John241198 <96462147+John241198@users.noreply.github.com>
2022-07-21 18:45:30 +0530

    Update Q58_Length of Last Word.py

--- Q58_Length of Last Word.py
+++ Q58_Length of Last Word.py
@@ -1,11 +1,13 @@
 s = "   fly me   to   the moon  "
 
 #initial solution
+"""
+initially declare an empty list as res in for loop split the sentence and append in res using slice find 
+the last words and using len function we can find the length of last word.
+"""
 res = []
-
 for i in s.split():
     res.append(i)
-
 word = res[-1]
 length = len(word)
 print(length)
